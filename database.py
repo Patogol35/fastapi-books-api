7bd02ce5bfd3d1afe856f7a5941c0faf5d15d83c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-10-12 18:06:20 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configura tu conexión a MySQL
+# Configura conexión a MySQL
 DATABASE_URL = "mysql+pymysql://root:patricio12@localhost:3306/fastapi_books"
 
 
